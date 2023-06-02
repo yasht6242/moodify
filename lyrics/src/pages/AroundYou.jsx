6a792c63f9e906a787ab35c522e43445712e21f8
yasht6242 <74258946+yasht6242@users.noreply.github.com>
2023-06-03 00:29:47 +0530

    first commit

---
+++ lyrics/src/pages/AroundYou.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+import { Error, Loader, SongCard } from "../components";
+import { useGetSongsByCountryQuery } from "../redux/services/shazamCore";
+
+const CountryTracks = () => {
+  const [country, setCountry] = useState("");
+  const [loading, setLoading] = useState(true);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsByCountryQuery("IN");
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.geoapify.com/v1/ipinfo?&apiKey=5ac457c4b9854e15b58d8a5188107cd6`
+      )
+      .then((res) => setCountry(res?.data?.location.country))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, [country]);
+
+  if (isFetching && loading)
+    return <Loader title="Loading Songs around you..." />;
+
+  if (error && country !== "") return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Around you <span className="font-black">{country}</span>
+      </h2>
+
+      <div className="flex flex-wrap sm:mt-0 mt-5 justify-center gap-10 ">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CountryTracks;
